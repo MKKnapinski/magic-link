74fc1faae6c341b3efecc6d2365699161033d8f4
Michał Knapiński <mkknapinski@gmail.com>
2022-08-05 17:53:54 +0200

    Email sender integration

--- magic-link/src/main/java/com/example/magiclink/MagicLinkApplication.java
+++ magic-link/src/main/java/com/example/magiclink/MagicLinkApplication.java
@@ -1,13 +1,25 @@
 package com.example.magiclink;
 
+import com.example.magiclink.mail.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MagicLinkApplication {
+
+    @Autowired
+    EmailService emailService;
 
     public static void main(String[] args) {
         SpringApplication.run(MagicLinkApplication.class, args);
     }
 
+    @GetMapping
+    void test() {
+        emailService.sendSimpleMessage("mkknapinski@gmail.com", "test", "test");
+    }
 }
