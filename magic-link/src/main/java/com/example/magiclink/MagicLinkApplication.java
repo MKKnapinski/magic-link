b6f68dfe494f60ce8f724211f185388118102ce2
Michał Knapiński <mkknapinski@gmail.com>
2022-08-05 17:16:49 +0200

    generated project

---
+++ magic-link/src/main/java/com/example/magiclink/MagicLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.magiclink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicLinkApplication.class, args);
+    }
+
+}
---
+++ magic-link/src/test/java/com/example/magiclink/MagicLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.magiclink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
